88fd42a2cfa614dd5e527429a46ed57a27068b15
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-12-10 14:18:32 +0100

    AJOUT CLASSE ENTIEE

---
+++ UCCMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaniqueEnCuisine
+{
+    /// <summary>
+    /// Logique d'interaction pour UCCMenu.xaml
+    /// </summary>
+    public partial class UCCMenu : Page
+    {
+        public UCCMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
