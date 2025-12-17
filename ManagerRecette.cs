1ad85199aee16a9e4d59d8ddf3de4363ab868281
Basile <Basile@HPBASILE>
2025-12-17 01:11:21 +0100

    ajout de recette pizza

--- ManagerRecette.cs
+++ ManagerRecette.cs
@@ -12,12 +12,30 @@ namespace PaniqueEnCuisine
 
         public ManagerRecette()
         {
-            
+            // création des ingrédiant 
+            Nouriture tomate = new Nouriture("tomate", "ingrediant");
+            Nouriture fromage = new Nouriture("fromage", "ingrediant");
+
+            // création des plat 
+            Nouriture pizza = new Nouriture("pizza", "plat");
+
+            // création des Recettes 
+            CreeRecette(pizza,tomate,fromage);
+
         }
 
         private void AjoutRecette(Recette newRecette)
         {
             ListeRecetes.Add(newRecette);
+        }
+        private void CreeRecette(Nouriture plat, Nouriture ingrédiant, Nouriture ingrédiant1)
+        {
+            
+            List<Nouriture> ingrediant = new List<Nouriture>();
+            ingrediant.Add(ingrédiant);
+            ingrediant.Add(ingrédiant1);
+            Recette RecettePizza = new Recette(ingrediant, plat);
+            AjoutRecette(RecettePizza);
         }
 
         internal List<Recette> ListeRecetes
